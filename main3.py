3f9cfe6d73c137845837044f15389a47123122e9
Nikita12712 <ndvor6667@gmail.com>
2024-08-23 19:55:31 +0300

    Add files via upload

--- main3.py
+++ main3.py
@@ -1,14 +1,10 @@
-number = int(input("Введіть 5-ти значне число: "))
+import random
 
+list_length = random.randint(3, 10)
 
-n1 = number % 10
-n2 = (number // 10) % 10
-n3 = (number // 100) % 10
-n4 = (number // 1000) % 10
-n5 = number // 10000
+my_list = [random.randint(1, 100) for _ in range(list_length)]
 
+new_list = [my_list[0], my_list[2], my_list[-2]]
 
-reversed_number = n1 * 10000 + n2 * 1000 + n3 * 100 + n4 * 10 + n5
-
-
-print("Перевернуте число:", reversed_number)
+print("Оригінальний список:", my_list)
+print("Новий список:", new_list)
