96d59542b6b4a945f0cf0c24054b1efab4448321
Nikita12712 <ndvor6667@gmail.com>
2024-08-30 22:31:30 +0300

    Add files via upload

--- lesson6.3.py
+++ lesson6.3.py
@@ -1,15 +1,15 @@
-def multiply_digits_until_single_digit(number):
+def multiply_digits_until_single_digit(number): #это смогу обьяснить
 
-    number = abs(number)
+    number = abs(number) #это не понял
 
-    while number > 9:
-        result = 1
-        for digit in str(number):
-            result *= int(digit)
-        number = result
+    while number > 9: #это смогу обьяснить
+        result = 1 #это смогу обьяснить
+        for digit in str(number): #это не понял
+            result *= int(digit) #это не понял
+        number = result #это смогу обьяснить
 
-    return number
+    return number #это смогу обьяснить
 
-user_input = int(input("Enter a number: "))
-result = multiply_digits_until_single_digit(user_input)
-print(result)
+user_input = int(input("Enter a number: ")) #это смогу обьяснить
+result = multiply_digits_until_single_digit(user_input) #это смогу обьяснить
+print(result) #это смогу обьяснить
