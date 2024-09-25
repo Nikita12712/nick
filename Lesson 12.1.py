a2498caf298a504090f5c29263b743540ed85968
Nikita12712 <ndvor6667@gmail.com>
2024-09-25 20:30:15 +0300

    Add files via upload

---
+++ Lesson 12.1.py
@@ -0,0 +1,14 @@
+import codecs
+import re
+
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+    with codecs.open(html_file, 'r', 'utf-8') as file:
+        html_content = file.read()
+
+    cleaned_text = re.sub(r'<[^>]+>', '', html_content)
+
+    cleaned_lines = [line.strip() for line in cleaned_text.splitlines() if line.strip()]
+
+    with codecs.open(result_file, 'w', 'utf-8') as output_file:
+        output_file.write("\n".join(cleaned_lines))
+
